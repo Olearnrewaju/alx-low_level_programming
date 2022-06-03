410a83373a4f22d7abdf427fdeb3361b0df5c4eb
olearnrewaju <awoyeleolanrewaju@gmail.com>
2022-06-02 21:58:33 -0700

    omoo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *
+ * 	* main - entry point
+ *
+ * 		* this is a function that prints a single line of code
+ *
+ * 			*
+ *
+ * 				* Return: it is going to always retun 0
+ *
+ * 				*/
+
+int main(void)
+	
+{
+	
+		puts("\"Programming is like building a multilingual puzzle");
+		
+			return (0);
+			
+				/* return - is 0 */
+			
+}
