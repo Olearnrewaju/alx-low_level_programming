45b730597d3c858ce357ffe3015edf31e29be350
olearnrewaju <awoyeleolanrewaju@gmail.com>
2022-06-05 09:22:14 -0700

    well

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *	 main is the entry point
+ * 	Description: a C program that prints Using the function printf
+ * 	Return: 0 as the number
+ *
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+						return (0);
+
+}
