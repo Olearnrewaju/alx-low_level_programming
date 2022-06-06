5f56962339899638ee6185a287d50ed4b8e07857
olearnrewaju <awoyeleolanrewaju@gmail.com>
2022-06-06 07:28:02 -0700

    war

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all the alphabets in lower case
+ * Return: return all alphabets
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int coma = 1;
+
+	while (a <= 9)
+	{
+		while (b <= 9)
+		{
+			while (c <= 9)
+			{
+				if (a < b && b < c)
+				{
+					if (coma == 0)
+					{
+						putchar(',');
+						putchar(32);
+					}
+					coma = 0;
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+				}
+				++c;
+			}
+			++b;
+			c = 0;
+		}
+		++a;
+		b = 0;
+	}
+	putchar(10);
+	return (0);
+}
