7b0ea60ceff4058ec62cb9d675703faf4511a2fb
olearnrewaju <awoyeleolanrewaju@gmail.com>
2022-06-06 07:08:21 -0700

    okay

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all the alphabets in lower case
+ * Return: return all the alphabets
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int coma = 1;
+
+	while (a <= 9)
+	{
+		while (b <= 9)
+		{
+			if (a < b)
+			{
+				if (coma == 0)
+				{
+					putchar(',');
+					putchar(32);
+				}
+				coma = 0;
+				putchar(a + '0');
+				putchar(b + '0');
+			}
+			b++;
+		}
+		a++;
+		b = 0;
+	}
+	putchar(10);
+	return (0);
+}
